ce900250813f1e9da97caf366789ac7c56f00073
Cameron Wilby <cameron@origincodeacademy.com>
2016-08-29 13:50:27 -0700

    Add example project

---
+++ Example Projects/14-TopSpotsAPI/server/Todo.API/Controllers/TodosController.cs
@@ -0,0 +1,114 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Todo.API.Infrastructure;
+
+namespace Todo.API.Controllers
+{
+    public class TodosController : ApiController
+    {
+        private TodoDataContext db = new TodoDataContext();
+
+        // GET: api/Todos
+        public IQueryable<Models.Todo> GetTodoes()
+        {
+            return db.Todoes;
+        }
+
+        // GET: api/Todos/5
+        [ResponseType(typeof(Models.Todo))]
+        public IHttpActionResult GetTodo(int id)
+        {
+            Models.Todo todo = db.Todoes.Find(id);
+            if (todo == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(todo);
+        }
+
+        // PUT: api/Todos/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutTodo(int id, Models.Todo todo)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != todo.TodoId)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(todo).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TodoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Todos
+        [ResponseType(typeof(Models.Todo))]
+        public IHttpActionResult PostTodo(Models.Todo todo)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Todoes.Add(todo);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = todo.TodoId }, todo);
+        }
+
+        // DELETE: api/Todos/5
+        [ResponseType(typeof(Models.Todo))]
+        public IHttpActionResult DeleteTodo(int id)
+        {
+            Models.Todo todo = db.Todoes.Find(id);
+            if (todo == null)
+            {
+                return NotFound();
+            }
+
+            db.Todoes.Remove(todo);
+            db.SaveChanges();
+
+            return Ok(todo);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool TodoExists(int id)
+        {
+            return db.Todoes.Count(e => e.TodoId == id) > 0;
+        }
+    }
+}
---
+++ Example Projects/14-TopSpotsAPI/server/Todo.API/Infrastructure/TodoDataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Todo.API.Infrastructure
+{
+    public class TodoDataContext : DbContext
+    {
+        public TodoDataContext() : base("Todo")
+        {
+
+        }
+
+        public IDbSet<Todo.API.Models.Todo> Todoes { get; set; }
+    }
+}
