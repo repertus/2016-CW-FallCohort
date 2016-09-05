e36fe4d7469b59150e57d43d9af8906612004454
Cameron Wilby <cameron@origincodeacademy.com>
2016-09-05 14:20:29 -0700

    File changes

---
+++ Example Projects/14-TodoAPI/server/Todo.API/Models/Todo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Todo.API.Models
+{
+    public class Todo
+    {
+        public int TodoId { get; set; }
+        public string Name { get; set; }
+        public int Priority { get; set; }
+    }
+}
